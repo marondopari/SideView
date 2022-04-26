210dc4121b63b1827a0c864852b0f04741677ce2
marondopari <mikaexo930@gmail.com>
2022-04-26 17:01:44 +0900

    PlayerController.csにアニメーション処理追加

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -13,16 +13,40 @@ public class PlayerController : MonoBehaviour
     bool goJump = false;            //ジャンプ開始フラグ
     bool onGround = false;          //地面に立っているフラグ
 
+    //アニメーション対応
+    Animator animator;  //アニメーター
+    public string stopAnime = "PlayerStop";
+    public string moveAnime = "PlayerMove";
+    public string jumpAnime = "PlayerJump";
+    public string goalAnime = "PlayerGoal";
+    public string deadAnime = "PlayerOver";
+    string nowAnime = "";
+    string oldAnime = "";
+
+    public static string gameState = "Playing"; //ゲームの状態
+
     // Start is called before the first frame update
     void Start()
     {
         //Rigidbody2Dを取ってくる
         rbody = this.GetComponent<Rigidbody2D>();
+
+        //animatorを取ってくる
+        animator = GetComponent<Animator>();
+        nowAnime = stopAnime;
+        oldAnime = stopAnime;
+
+        gameState = "Playing";      //ゲーム中にする
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(gameState!="Playing")
+        {
+            return;
+        }
+
         //水平方向の入力をチェックする
         axisH = Input.GetAxisRaw("Horizontal");
 
@@ -49,6 +73,11 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+        if(gameState!="Playing")
+        {
+            return;
+        }
+
         //地上判定
         onGround = Physics2D.Linecast(transform.position,
             transform.position - (transform.up * 0.1f), groundLayer);
@@ -68,6 +97,29 @@ public class PlayerController : MonoBehaviour
             rbody.AddForce(jumpPw, ForceMode2D.Impulse);    //瞬間的な力を加える
             goJump = false;     //ジャンプフラグをおろす
         }
+
+        if(onGround)
+        {
+            //地面の上
+            if(axisH==0)
+            {
+                nowAnime = stopAnime;   //停止中
+            }
+            else
+            {
+                nowAnime = moveAnime;   //移動
+            }
+        }
+        //空中
+        else
+        {
+            nowAnime = jumpAnime;
+        }
+        if(nowAnime!=oldAnime)
+        {
+            oldAnime = nowAnime;
+            animator.Play(nowAnime);    //アニメーション再生
+        }
     }
 
     //ジャンプ
@@ -75,5 +127,51 @@ public class PlayerController : MonoBehaviour
     {
         goJump = true;  //ジャンプフラグをたてる
         Debug.Log("ジャンプボタン押し");
+    }
+
+    //接触開始
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag=="Goal")
+        {
+            Goal(); //ゴール
+        }
+        else if(collision.gameObject.tag=="Dead")
+        {
+            GameOver();
+        }
+    }
+
+    //ゴール
+    public void Goal()
+    {
+        animator.Play(goalAnime);
+
+        gameState = "gameclear";
+        GameStop();  //ゲーム停止
+    }
+
+    //ゲームオーバー
+    public void GameOver()
+    {
+        animator.Play(deadAnime);
+
+        gameState = "gameover";
+        GameStop(); //ゲーム停止
+
+        //ゲームオーバー演出
+        //プレイヤーあたりを消す
+        GetComponent<CapsuleCollider2D>().enabled = false;
+        //プレイヤーを上に少し跳ね上げる演出
+        rbody.AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+    }
+
+    //ゲーム停止
+    void GameStop()
+    {
+        //リジッドボディ2Dを取ってくる
+        Rigidbody2D rbody = GetComponent<Rigidbody2D>();
+        //速度を０にして強制停止
+        rbody.velocity = new Vector2(0, 0);
     }
 }
